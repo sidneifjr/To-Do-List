8e140058010e7ae23affc41fe54fe034d7483ca2
Sidnei Farias <sidneijr.77@gmail.com>
2023-01-30 23:40:46 -0300

    Inclui um fail case para adição de item inválido e sua tentativa de correção, em test1.cy.ts

--- cypress/e2e/test1.cy.ts
+++ cypress/e2e/test1.cy.ts
@@ -4,17 +4,33 @@ describe('Ciclo de vida do item', () => {
     cy.viewport(1920, 1080)
   })
 
-  // Teste sequencial.
+  // Happy path.
   it('Adiciona um novo item', () => {
-    let text = "Ir ao mercado e comprar verduras";
-
     // Adiciona um item.
-    cy.get('form').click().type(text).get('button[type="submit"]').click()
+    cy.get('form').click().type("Ir ao mercado e comprar verduras")
+    
+    cy.get('button[type="submit"]').click()
 
     // Marca o item recém-adicionado.
     cy.get('.Lists-sc-gqgo16-1 > :nth-child(7) > input').click()
 
     // Exclui o item.
+    cy.get('.active > .DeleteButton-sc-1813rzx-1 > img').wait(1200).click()
+  })
+
+  // Fail case, então corrigido para um happy path.
+  it('Adiciona um item inválido', () => {
+    cy.get('form').click().type(" ")
+
+    cy.get('button[type="submit"]').click()
+
+    // Tenta resolver o erro, cadastrando um novo valor.
+    cy.get('form').click().type("testando validade")
+
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.Lists-sc-gqgo16-1 > :nth-child(7) > input').click()
+
     cy.get('.active > .DeleteButton-sc-1813rzx-1 > img').wait(1200).click()
   })
 })
